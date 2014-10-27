795f059706b1499578e326b269b3013a3a184aad
Paul <wyattpj@email.appstate.edu>
2014-10-26 20:11:12 -0400

    File serialization, cleanup, formatting
    Added file serialization, did some code clean up, formatted output to
    reddit styled tables.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 gtpulse
 =======
+
+Download/install python2.7
+Download/install pip
+pip install praw
+
+./pulse.py
--- pulse.py
+++ pulse.py
@@ -3,19 +3,25 @@ import praw
 import re
 import sys
 import os
+import json
 
 class Pulse(object):
-    pass
-      
-
-class PageParse(object):
     def __init__(self, page):
         self.page = page
         print page
+        self.last_comment_id = None
         self.reddit = praw.Reddit('thangs')
-        self.submission = self.reddit.get_submission(page)
+        self.submission = self.reddit.get_submission(page, comment_sort='new')
+        self.title = str(self.submission.title).replace(' ','_').replace(
+            '-','').replace(':','').replace('~','').replace('(','').replace(
+            ')','')[0:32]
+        print self.title
+        self.gdt_file ='%s.txt' % os.path.join(os.path.expanduser('~'), self.title)
         self.pulse_dict = {}
-        self.uniques = []
+        self.uniques = set()
+        self.comment_ids = set()
+        self.read_shit()
+
         
     def run(self):
         self.submission.replace_more_comments(limit=None, threshold=0)
@@ -28,44 +34,21 @@ class PageParse(object):
         self.fuck = re.compile('fu*ck')
         self._class = re.compile('class.*')
 
-        """while True:
-            print flat_comments[1]
-
-            if len(flat_comments) == 0:
-                break"""
+        self.latest_comment_id = flat_comments[0].id
+        print self.latest_comment_id
+        print self.last_comment_id
+        
         for idx, comment in enumerate(flat_comments):
+            print comment.id
+            if comment.id in self.comment_ids:
+                continue
+            self.comment_ids.add(comment.id)
             self.go(comment)
             if (idx % 100) == 0:
                 self.print_shit()
 
         self.print_shit(done=True)
         print 'finished. %s comments processed' % idx
-
-        "self.submission.replace_more_comments()"
-            
-
-        ":fucks = get_fucks(comment)"
-
-        """outstring = "\n".join(outstring.format(
-            team=k, fucks=x['fucks'], _class=x['class'],
-            by_team=x['count'], by_flair=x['unique']) for k,x in
-            self.pulse_dict.iteritems())"""
-        
-        
-        """print "Number of fucks by flair:  "
-        for k,v in self.pulse_dict.iteritems():
-            print "%s:\t%s  " % (k,v['fucks'])
-        print " "
-        print "Number of unique users by flair:"
-        for k,v in self.pulse_dict.iteritems():
-            print "%s:\t%s  " % (k,v['unique'])
-        print " "
-        print "Number of posts by flair:  "
-
-        for k,v in self.pulse_dict.iteritems():
-            print "%s:\t%s  " % (k,v['count'])
-        print " "
-        print "Number of unique users: %s" % len(self.uniques)"""
 
     def go(self, comment):
         try:
@@ -84,8 +67,8 @@ class PageParse(object):
         if self._class.search(comment.body.lower()):
             self.pulse_dict[aft]['class'] += 1
             
-        if comment.author not in self.uniques:
-            self.uniques.append(comment.author)
+        if str(comment.author) not in self.uniques:
+            self.uniques.add(str(comment.author))
             self.pulse_dict[aft]['unique'] += 1
             
         self.pulse_dict[aft]['count'] += 1
@@ -97,16 +80,37 @@ class PageParse(object):
         by_team=x['count'], by_flair=x['unique']) for k,x in
         self.pulse_dict.iteritems())
         if done:
-            with open('%s.txt' % self.page.split('/')[6], 'wb') as p:
-                print >> p, self.outstring_header
-                print >> p, outstring
+            print 'Writing to %s' % self.gdt_file
+            with open(self.gdt_file, 'wb') as p:
+                p.write(json.dumps(self.pulse_dict) + '\n')
+                p.write('Comment: %s' % self.latest_comment_id + '\n')
+                p.write(json.dumps(list(self.uniques)) + '\n')
+                p.write(json.dumps(list(self.comment_ids)) +  '\n')
+                p.write(self.outstring_header + '\n')
+                p.write(outstring)
         print self.outstring_header
         print outstring
+
+    def read_shit(self):
+
+        try:
+            with open(self.gdt_file, 'rb') as p:
+                print 'Reading from %s' % self.gdt_file
+                json_dict = p.readline()
+                self.pulse_dict = dict(json.loads(json_dict))
+                comment = p.readline()
+                if comment.startswith('Comment'):
+                    self.last_comment_id = comment.split(' ')[1]
+                self.uniques = set(json.loads(p.readline()))
+                self.comment_ids = set(json.loads(p.readline()))
+        except Exception, e:
+            print e
+            print 'No file found'
 
 print 'GDT address?'
 page = str(raw_input("> "))
 
 
-parser = PageParse(page)
+parser = Pulse(page)
 parser.run()
 
